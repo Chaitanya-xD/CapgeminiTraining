1a8328b7fbdd8e38bcca9351adff463a8c754a18
Chaitanya-xD <chaitanyathumma29@gmail.com>
2025-01-25 19:18:47 +0530

    fizzbuzz correction

--- pythonExam/fizzbuzz.py
+++ pythonExam/fizzbuzz.py
@@ -1,35 +1,21 @@
 def fizz():
-  print("Multiples of 3 under 100 are : ")
-  for i in range(1,101):
-    if( i % 3 == 0 ):
-      print(i,end=' ')
-  print()
+  print("fizz",end=' ')
   
 def buzz():
-  print("Multiples of 5 under 100 are : ")
-  for i in range(1,101):
-    if( i % 5 == 0 ):
-      print(i,end=' ')
-  print()
+  print("buzz",end=' ')
 
 def fizzbuzz():
-  print("Multiples of 3 and 5 under 100 are : ")
-  for i in range(1,101):
-    if( i % 3 == 0 and i % 5 == 0 ):
-      print(i,end=' ')
-  print()
-
+  print("fizzbuzz",end=' ')
+  
   
 def main():
-  x = input("Pick one:\nFizz\nBuzz\nFizzBuzz\n")
-  x = x.lower()
-
-  if( x == 'fizz'):
-    fizz()
-  elif( x == 'buzz' ):
-    buzz()
-  elif( x == 'fizzbuzz'):
-    fizzbuzz()
-  else:
-    print("Enter valid option!")
+  for i in range(1,101):
+    if( i % 3 == 0 and i % 5 == 0 ):
+      fizzbuzz()
+    elif( i % 5 == 0 ):
+      buzz()
+    elif i % 3 == 0 :
+      fizz()
+    else:
+      print(i,end=" ")
 main()
